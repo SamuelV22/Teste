64bc08975756a8f227dec768d50782999b3cd614
SamuelV22 <samuelv22@protonmail.com>
2021-03-26 19:14:16 -0300

    adição das páginas de teste

---
+++ codigo-js/codigo-js.js
@@ -0,0 +1,21 @@
+console.log("teste git bash ");
+
+
+var objJose = {
+
+    nome: "José Guilherme" ,
+    idade: 23 ,
+    joseFuncao: function (){
+        console.log("Ele está me ajudando");
+    }
+
+};
+
+console.log("Acessando José ...");
+console.log(objJose);
+
+console.log("Nome : " + objJose.nome);
+console.log("Idade: " + objJose.idade);
+
+console.log("Acessando função de José ...");
+objJose.joseFuncao();
